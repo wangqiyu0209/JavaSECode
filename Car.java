2481d0f9dc1adab42a115e75f37df4b252d02abc
wangqiyu0209 <48886012+wangqiyu0209@users.noreply.github.com>
2019-08-09 20:28:13 +0800

    2019/8/9
    定义一个一般方法run()，用打印语句描述汽车奔跑的功能，其main方法中创建一个品牌为“benz”、颜色为“black”的汽车。

---
+++ Car.java
@@ -0,0 +1,37 @@
+﻿package com.neuedu.test2;
+
+/**
+* @ClassName: Car
+* @Description: TODO(这里用一句话描述这个类的作用)
+* @author wqy
+* @date 2019年8月9日 下午1:57:30
+*
+*/
+public class Car extends Vehicle{
+
+	private int loader;
+	
+	public Car() {
+		super();
+	}
+
+	// 构造方法的重载
+	public Car(String brand,String color,int loader,double speed) {
+		super(brand,color,speed);
+		this.loader = loader;
+	}
+	
+	// 重写方法run()
+	@Override
+	public void run() {
+		System.out.println("一俩"+super.getColor()+"的"+super.getBrand()
+		+"载着"+ loader+"个人，以"+super.getSpeed()+"km/h的速度，在飞驰");
+	}
+	
+	public static void main(String[] args) {
+		Car car = new Car("benz","black",0,2);
+		car.run();
+	}
+	
+	
+}
---
+++ Vehicle.java
@@ -0,0 +1,51 @@
+﻿package com.neuedu.test2;
+
+/**
+* @ClassName: Vehicle
+* @Description: TODO(这里用一句话描述这个类的作用)
+* @author wqy
+* @date 2019年8月9日 下午1:48:48
+*
+*/
+public class Vehicle {
+
+	private String brand;
+	private String color;
+	private double speed;
+	
+	public Vehicle(){
+		super();
+	}
+	public Vehicle(String brand, String color, double speed) {
+		super();
+		this.brand = brand;
+		this.color = color;
+		this.speed = speed;
+	}
+	
+	public void run() {
+		System.out.println("一辆颜色为"+color+"的"+brand+"以初速度"+speed+"起步");
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public double getSpeed() {
+		return speed;
+	}
+	
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+	
+
+}
